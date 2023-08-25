a10d8d3989151532ac639bf2b4bead21a1a51111
dzmitry_sidorov <jimmaggot@mail.ru>
2023-08-25 14:52:47 +0300

    Create login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CreatePaymentView from '../views/CreatePaymentView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: CreatePaymentView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
     // {
     //   path: '/about',
---
+++ src/views/LoginView.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-sheet class="mx-auto create-form teal-lighten-3">
+    <h4 class="header">Login:</h4>
+    <v-form validate-on="submit lazy" @submit.prevent>
+      <v-text-field
+        class="input"
+        label="Email"
+        type="email"
+        variant="outlined"
+        v-model="credentials.email"
+      />
+      <v-text-field
+        class="input"
+        label="Password"
+        type="password"
+        variant="outlined"
+        v-model="credentials.password"
+        />
+      <v-btn class="button" @click="login" variant="outlined">Login</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+
+<script lang="ts">
+
+type LoginView = {
+  email?: string;
+  password?: string;
+}
+
+const DEFAULT_CREDENTIALS: LoginView = {
+  email: undefined,
+  password: undefined,
+}
+
+export default {
+  data() {
+    return {
+      credentials: { ...DEFAULT_CREDENTIALS },
+    }
+  },
+  methods: {
+    login() {
+      this.$emit('create', this.credentials)
+    },
+  }
+}
+</script>
+
+
+<style>
+:root {
+  --main-color-light: rgba(0, 128, 128, 0.5);
+  --main-color: rgba(0, 128, 128, 1);
+}
+
+.create-form {
+  width: 600px;
+}
+
+.header {
+  margin: 10px 0;
+  font-size: 24px;
+}
+
+.input {
+  /* padding: 10px; */
+  /* max-height: 50px; */
+}
+
+.button {
+  width: 200px;
+  min-height: 60px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+/* .button {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  background-color: var(--main-color-light);
+  border: 1px solid black;
+  border-radius: 4px;
+} */
+
+button:hover {
+  background-color: var(--main-color);
+}
+
+.amount {
+  display: flex;
+  gap: 16px;
+}
+
+/* .input {
+  width: auto;
+  border: 1px solid var(--main-color-light);
+  padding: 10px 15px;
+  margin-top: 15px;
+  border-radius: 4px;
+} */
+
+.textarea {
+  height: 200%;
+}
+
+.input:hover {
+  border-color: var(--main-color);
+}
+
+.input:hover::placeholder {
+  color: var(--main-color);
+}
+</style>
